49830018ad5fa6af9c82f286a5bab73b3df1a808
mammamiiiya <25200001+mammamiiiya@users.noreply.github.com>
2021-06-05 03:11:51 -0400

    Changes

--- spotify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Installing Spotify
-
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
-apt update && apt install spotify-client -yqq
-#EOF
